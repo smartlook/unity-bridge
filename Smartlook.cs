aae0cfc8cf4914cb6b55d0701787b68608f53334
Artūras Šlajus <x11@arturaz.net>
2019-04-02 10:44:16 +0300

    Update Smartlook.cs

--- Smartlook.cs
+++ Smartlook.cs
@@ -1,165 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Runtime.InteropServices;
+namespace SmartLookUnity {
+  public static partial class SmartLook {
+    public static void Init(string key) { InitInternal(key); }
+    static partial void InitInternal(string key);
 
+    public static void Init(string key, int frameRate) { InitInternal(key, frameRate); }
+    static partial void InitInternal(string key, int frameRate);
 
-public class Smartlook
-{
-    public static AndroidJavaClass SL = null;
+    public static void RecordEvent(string eventName) { RecordEventInternal(eventName); }
+    static partial void RecordEventInternal(string eventName);
 
-    #if UNITY_IOS
-    [DllImport("__Internal")]
-    private static extern void SmartlookInit(string key);
-    #endif
+    public static void RecordEvent(string eventName, string properties) { RecordEventInternal(eventName, properties); }
+    static partial void RecordEventInternal(string eventName, string properties);
 
-    public static void Init(string key)
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-            SmartlookInit(key);
-        }
-        else
-        {
-            object[] parameters = new object[1];
-            parameters[0] = key;
-            getSLClass().CallStatic("init", parameters);
-        }
-    }
+    public static void TimeEvent(string eventName) { TimeEventInternal(eventName); }
+    static partial void TimeEventInternal(string eventName);
 
-    #if UNITY_IOS
-    [DllImport("__Internal")]
-    private static extern void SmartlookInitWithFramerate(string key, int framerate);
-    #endif
+    public static void SetUserIdentifier(string userIdentifier) { SetUserIdentifierInternal(userIdentifier); }
+    static partial void SetUserIdentifierInternal(string userIdentifier);
 
-    public static void Init(string key, int framerate)
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-            SmartlookInitWithFramerate(key, framerate);
-        }
-        else
-        {
-            object[] parameters = new object[2];
-            parameters[0] = key;
-            parameters[1] = framerate;
-            getSLClass().CallStatic("init", parameters);
-        }
-    }
+    public static void PauseRecording() { PauseRecordingInternal(); }
+    static partial void PauseRecordingInternal();
 
-    #if UNITY_IOS
-    [DllImport("__Internal")]
-    private static extern void SmartlookRecordEvent(string eventName);
-    #endif
-
-    public static void RecordEvent(string eventName)
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-            SmartlookRecordEvent(eventName);
-        }
-        else
-        {
-            object[] parameters = new object[1];
-            parameters[0] = eventName;
-            getSLClass().CallStatic("track", parameters);
-        }
-    }
-
-    #if UNITY_IOS
-    [DllImport("__Internal")]
-    private static extern void SmartlookRecordEventWithProperties(string eventName, string properties);
-    #endif
-
-    public static void RecordEvent(string eventName, string properties)
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-            SmartlookRecordEventWithProperties(eventName, properties);
-        }
-        else
-        {
-            object[] parameters = new object[2];
-            parameters[0] = eventName;
-            parameters[1] = properties;
-            getSLClass().CallStatic("track", parameters);
-        }
-    }
-
-    public static void TimeEvent(string eventName)
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-
-        }
-        else
-        {
-            object[] parameters = new object[1];
-            parameters[0] = eventName;
-            getSLClass().CallStatic("timeEvent", parameters);
-        }
-    }
-
-    #if UNITY_IOS
-    [DllImport("__Internal")]
-    private static extern void SmartlookSetUserIdentifier(string userIdentifier);
-    #endif
-
-    public static void SetUserIdentifier(string userIdentifier)
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-            SmartlookSetUserIdentifier(userIdentifier);
-        }
-        else
-        {
-            object[] parameters = new object[1];
-            parameters[0] = userIdentifier;
-            getSLClass().CallStatic("identify", parameters);
-        }
-    }
-
-
-    #if UNITY_IOS
-    [DllImport("__Internal")]
-    private static extern void SmartlookPauseRecording();
-    #endif
-
-    public static void PauseRecording()
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-            SmartlookPauseRecording();
-        }
-        else
-        {
-            getSLClass().CallStatic("pause");
-        }
-    }
-
-    #if UNITY_IOS
-    [DllImport("__Internal")]
-    private static extern void SmartlookResumeRecording();
-    #endif
-
-    public static void ResumeRecording()
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-            SmartlookResumeRecording();
-        }
-        else
-        {
-            getSLClass().CallStatic("start");
-        }
-    }
-
-    private static AndroidJavaClass getSLClass()
-    {
-        if (Smartlook.SL != null) {
-            return Smartlook.SL;
-        }
-        Smartlook.SL = new AndroidJavaClass("com.smartlook.sdk.smartlook.Smartlook");
-        return Smartlook.SL;
-    }
+    public static void ResumeRecording() { ResumeRecordingInternal(); }
+    static partial void ResumeRecordingInternal();
+  }
 }
