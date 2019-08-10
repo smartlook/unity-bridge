69d2442531a4ee2f95a95238a2dd85d1115e89aa
Jiri Charvat <m@jcharv.net>
2019-08-10 15:37:41 +0200

    iOS updated, docs.

--- Smartlook.IOS.cs
+++ Smartlook.IOS.cs
@@ -1,127 +1,180 @@
 #if UNITY_IOS
-using System.Runtime.InteropServices;
-using UnityEngine;
 
 namespace SmartlookUnity {
-  public partial class Smartlook {
+	using System.Runtime.InteropServices;
+	using UnityEngine;
+	
+	public partial class Smartlook {
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookSetupAndStartRecording(string key);
+		
+		static partial void SetupAndStartRecordingInternal(string key) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookSetupAndStartRecording(key);
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookSetupAndStartRecordingWithFramerate(string key, int frameRate);
+		
+		static partial void SetupAndStartRecordingInternal(string key, int frameRate) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookSetupAndStartRecordingWithFramerate(key, frameRate);
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookSetup(string key);
+		
+		static partial void SetupInternal(string key) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookSetup(key);
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookSetupWithFramerate(string key, int frameRate);
+		
+		static partial void SetupInternal(string key, int frameRate) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookSetupWithFramerate(key, frameRate);
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookStartRecording();
+		
+		static partial void StartRecordingInternal() {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookStartRecording();
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookStopRecording();
+		
+		static partial void StopRecordingInternal() {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookStopRecording();
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookStartFullscreenSensitiveMode();
+		
+		static partial void StartFullscreenSensitiveModeInternal() {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookStartFullscreenSensitiveMode();
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookStopFullscreenSensitiveMode();
+		
+		static partial void StopFullscreenSensitiveModeInternal() {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookStopFullscreenSensitiveMode();
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookSetReferrer(string referrer, string source);
+		
+		static partial void SetReferrerInternal(string referrer, string source) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookSetReferrer(referrer, source);
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookInit(string key);
+		
+		static partial void InitInternal(string key) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookInit(key);
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookInitWithFramerate(string key, int framerate);
+		
+		static partial void InitInternal(string key, int frameRate) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookInitWithFramerate(key, frameRate);
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookRecordEvent(string eventName);
+		
+		static partial void RecordEventInternal(string eventName) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookRecordEvent(eventName);
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookRecordEventWithProperties(string eventName, string properties);
+		
+		static partial void RecordEventInternal(string eventName, string properties) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookRecordEventWithProperties(eventName, properties);
+			}
+		}
 
-    static partial void SetupAndStartRecordingInternal(string key) {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        
-      }
-    }
+		[DllImport("__Internal")]
+		static extern void SmartlookTimeEvent(string eventName);
+		
+		static partial void TimeEventInternal(string eventName) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookTimeEvent(eventName);
+			}
+		}
 
-    static partial void SetupAndStartRecordingInternal(string key, int frameRate) {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        
-      }
-    }
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookSetUserIdentifier(string userIdentifier);
+		
+		static partial void SetUserIdentifierInternal(string userIdentifier) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookSetUserIdentifier(userIdentifier);
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookPauseRecording();
+		
+		static partial void PauseRecordingInternal() {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookPauseRecording();
+			}
+		}
+		
+		[DllImport("__Internal")]
+		static extern void SmartlookResumeRecording();
+		
+		static partial void ResumeRecordingInternal() {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				SmartlookResumeRecording();
+			}
+		}
 
-    static partial void SetupInternal(string key) {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        
-      }
-    }
+		[DllImport("__Internal")]
+		static extern bool SmartlookIsRecording();
 
-    static partial void SetupInternal(string key, int frameRate) {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        
-      }
-    }
+		public static bool IsRecordingInternalIOS() {
+			return SmartlookIsRecording();
+		}
 
-    static partial void StartFullscreenSensitiveModeInternal() {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        
-      }
-    }
+		[DllImport("__Internal")]
+		static extern string SmartlookGetDashboardSessionUrl();
 
-    static partial void StopFullscreenSensitiveModeInternal() {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        
-      }
-    }
+		public static string GetDashboardSessionUrlInternalIOS() {
+			return SmartlookGetDashboardSessionUrl();
+		}
 
-    static partial void SetReferrerInternal(string referrer, string source) {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        
-      }
-    }
-
-    static partial void StartRecordingInternal() {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        
-      }
-    }
-
-    static partial void StopRecordingInternal() {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        
-      }
-    }
-
-    [DllImport("__Internal")]
-    static extern void SmartlookInit(string key);
-
-    static partial void InitInternal(string key) { 
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        SmartlookInit(key);
-      }
-    }
-
-    [DllImport("__Internal")]
-    static extern void SmartlookInitWithFramerate(string key, int framerate);
-
-    static partial void InitInternal(string key, int frameRate) { 
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        SmartlookInitWithFramerate(key, frameRate);
-      }
-    }
-    
-    [DllImport("__Internal")] 
-    static extern void SmartlookRecordEvent(string eventName);
-    
-    static partial void RecordEventInternal(string eventName) { 
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        SmartlookRecordEvent(eventName);
-      }
-    }
-
-
-    [DllImport("__Internal")]
-    static extern void SmartlookRecordEventWithProperties(string eventName, string properties);
-
-    static partial void RecordEventInternal(string eventName, string properties) {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        SmartlookRecordEventWithProperties(eventName, properties);
-      }
-    }
-    
-    [DllImport("__Internal")]
-    static extern void SmartlookSetUserIdentifier(string userIdentifier);
-
-    static partial void SetUserIdentifierInternal(string userIdentifier) {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        SmartlookSetUserIdentifier(userIdentifier);
-      }
-    }
-    
-    [DllImport("__Internal")]
-    static extern void SmartlookPauseRecording();
-    
-    static partial void PauseRecordingInternal() { 
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        SmartlookPauseRecording();
-      }
-    }
-    
-    [DllImport("__Internal")]
-    static extern void SmartlookResumeRecording();
-
-    static partial void ResumeRecordingInternal() {
-      if (Application.platform == RuntimePlatform.IPhonePlayer) {
-        SmartlookResumeRecording();
-      }
-    }
-  }
+	}
 }
+
 #endif
--- Smartlook.cs
+++ Smartlook.cs
@@ -1,78 +1,125 @@
 using UnityEngine;
 
 namespace SmartlookUnity {
-  public static partial class Smartlook {
+    public static partial class Smartlook {
 
-    public static void SetupAndStartRecording(string key) { SetupAndStartRecordingInternal(key); }
-    static partial void SetupAndStartRecordingInternal(string key);
+        // Setup Smartlook and start recording.
+        /// <param name="key">The application (project) specific SDK key, available in your Smartlook dashboard.</param>
+        public static void SetupAndStartRecording(string key) { SetupAndStartRecordingInternal(key); }
 
-    public static void SetupAndStartRecording(string key, int frameRate) { SetupAndStartRecordingInternal(key, frameRate); }
-    static partial void SetupAndStartRecordingInternal(string key, int frameRate);
+        // Setup Smartlook and start recording, with custom framerate.
+        /// <param name="key">The application (project) specific SDK key, available in your Smartlook dashboard.</param>
+        /// <param name="frameRate">Custom recording framerate.</param> 
+        public static void SetupAndStartRecording(string key, int frameRate) { SetupAndStartRecordingInternal(key, frameRate); }
 
-    public static void Setup(string key) { SetupInternal(key); }
-    static partial void SetupInternal(string key);
+        // Setup Smartlook. This method initializes Smartlook SDK, but does not start recording. To start recording, call StartRecording() method.
+        /// <param name="key">The application (project) specific SDK key, available in your Smartlook dashboard.</param>
+        public static void Setup(string key) { SetupInternal(key); }
 
-    public static void Setup(string key, int frameRate) { SetupInternal(key, frameRate); }
-    static partial void SetupInternal(string key, int frameRate);
+        // Setup Smartlook. This method initializes Smartlook SDK, but does not start recording. To start recording, call StartRecording() method.
+        /// <param name="key">The application (project) specific SDK key, available in your Smartlook dashboard.</param>
+        /// <param name="frameRate">Custom recording framerate.</param> 
+        public static void Setup(string key, int frameRate) { SetupInternal(key, frameRate); }
 
-    public static bool IsRecording() {
-            if (Application.platform == RuntimePlatform.Android)
-            {
+        // Starts video and events recording.
+        public static void StartRecording() { StartRecordingInternal(); }
+
+        // Stops video and events recording.
+        public static void StopRecording() { StopRecordingInternal(); }
+
+        // Current video and events recording state.
+        public static bool IsRecording() { return IsRecordingInternal(); }
+
+        // Records timestamped custom event.
+        /// <param name="eventName">Name that identifies the event.</param>
+        public static void RecordEvent(string eventName) { RecordEventInternal(eventName); }
+
+        // Records timestamped custom event with optional properties.
+        /// <param name="eventName">Name that identifies the event.</param>
+        /// <param name="properties">Optional dictionary (json string, obtained for example with JsonUtility.ToJson(param)) with additional information. Non String values will be stringlified.</param>
+        public static void RecordEvent(string eventName, string properties) { RecordEventInternal(eventName, properties); }
+
+        // Returns URL leading to the Dashboard player for the current Smartlook session. This URL can be access by everyone with the access rights to the dashboard.
+        public static string GetDashboardSessionUrl() { return GetDashboardSessionUrlInternal(); }
+
+        // Use this method to enter the **full sensitive mode**. No video is recorded, just analytics events.
+        public static void StartFullscreenSensitiveMode() { StartFullscreenSensitiveModeInternal(); }
+
+        // Use this method to leave the **full sensitive mode**. Video is recorded again.
+        public static void StopFullscreenSensitiveMode() { StopFullscreenSensitiveModeInternal(); }
+
+
+        public static void SetReferrer(string referrer, string source) { SetReferrerInternal(referrer, source); }
+
+        /**
+            Start timer for custom event.
+         
+            This method does not record an event. It is the subsequent `RecordEvent` call with the same `eventName` that does.
+
+            In the resulting event, the property dictionaries of `start` and `record` are merged (the `record` values override the `start` ones if the key is the same), and a `duration` property is added to them.
+         
+         @param eventName Name of the event.
+         */
+         /// <param name="eventName">Name of the event.</param>
+         public static void TimeEvent(string eventName) { TimeEventInternal(eventName); }
+
+        // Set the app's user identifier.
+        /// <param name="userIdentifier">The application-specific user identifier.</param>
+         public static void SetUserIdentifier(string userIdentifier) { SetUserIdentifierInternal(userIdentifier); }
+
+
+        // Deprecated methods
+         public static void Init(string key) { InitInternal(key); }
+         public static void Init(string key, int frameRate) { InitInternal(key, frameRate); }
+         public static void PauseRecording() { PauseRecordingInternal(); }
+         public static void ResumeRecording() { ResumeRecordingInternal(); }
+         
+
+        // Internal
+         static partial void SetupAndStartRecordingInternal(string key);
+         static partial void SetupAndStartRecordingInternal(string key, int frameRate);
+         static partial void SetupInternal(string key);
+         static partial void SetupInternal(string key, int frameRate);
+         static partial void StartRecordingInternal();
+         static partial void StopRecordingInternal();
+         static partial void RecordEventInternal(string eventName);
+         static partial void RecordEventInternal(string eventName, string properties);
+         static partial void StartFullscreenSensitiveModeInternal();
+         static partial void StopFullscreenSensitiveModeInternal();
+         static partial void SetReferrerInternal(string referrer, string source);
+         static partial void TimeEventInternal(string eventName);
+         static partial void SetUserIdentifierInternal(string userIdentifier);
+
+         static partial void InitInternal(string key);
+         static partial void InitInternal(string key, int frameRate);
+         static partial void PauseRecordingInternal();
+         static partial void ResumeRecordingInternal();
+
+
+         public static bool IsRecordingInternal() {
+#if UNITY_ANDROID            
+            if (Application.platform == RuntimePlatform.Android) {
                 return getSLClass().CallStatic<bool>("isRecording");
-            } else
-            {
-                return true;
             }
+#elif UNITY_IOS
+            if (Application.platform == RuntimePlatform.IPhonePlayer) {
+                return IsRecordingInternalIOS();
+            }
+#endif
+            return false;
         }
 
-        public static string GetDashboardSessionUrl() {
-            if (Application.platform == RuntimePlatform.Android)
-            {
+        public static string GetDashboardSessionUrlInternal() {
+#if UNITY_ANDROID            
+            if (Application.platform == RuntimePlatform.Android) {
                 return getSLClass().CallStatic<string>("getDashboardSessionUrl");
             }
-            else
-            {
-                return "";
+#elif UNITY_IOS
+            if (Application.platform == RuntimePlatform.IPhonePlayer) {
+                return GetDashboardSessionUrlInternalIOS();
             }
+#endif
+            return "";
         }
-
-    public static void StartFullscreenSensitiveMode() { StartFullscreenSensitiveModeInternal(); }
-    static partial void StartFullscreenSensitiveModeInternal();
-
-    public static void StopFullscreenSensitiveMode() { StopFullscreenSensitiveModeInternal(); }
-    static partial void StopFullscreenSensitiveModeInternal();
-
-    public static void SetReferrer(string referrer, string source) { SetReferrerInternal(referrer, source); }
-    static partial void SetReferrerInternal(string referrer, string source);
-
-    public static void Init(string key) { InitInternal(key); }
-    static partial void InitInternal(string key);
-
-    public static void Init(string key, int frameRate) { InitInternal(key, frameRate); }
-    static partial void InitInternal(string key, int frameRate);
-
-    public static void RecordEvent(string eventName) { RecordEventInternal(eventName); }
-    static partial void RecordEventInternal(string eventName);
-
-    public static void RecordEvent(string eventName, string properties) { RecordEventInternal(eventName, properties); }
-    static partial void RecordEventInternal(string eventName, string properties);
-
-    public static void TimeEvent(string eventName) { TimeEventInternal(eventName); }
-    static partial void TimeEventInternal(string eventName);
-
-    public static void SetUserIdentifier(string userIdentifier) { SetUserIdentifierInternal(userIdentifier); }
-    static partial void SetUserIdentifierInternal(string userIdentifier);
-
-    public static void PauseRecording() { PauseRecordingInternal(); }
-    static partial void PauseRecordingInternal();
-
-    public static void ResumeRecording() { ResumeRecordingInternal(); }
-    static partial void ResumeRecordingInternal();
-
-    public static void StartRecording() { StartRecordingInternal(); }
-    static partial void StartRecordingInternal();
-
-    public static void StopRecording() { StopRecordingInternal(); }
-    static partial void StopRecordingInternal();
-  }
+    }
 }
